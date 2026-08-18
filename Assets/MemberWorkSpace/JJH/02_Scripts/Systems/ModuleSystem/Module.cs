c9243d8e253befc9f8d5765d3138dae6688a48d8
ForRest1217 <Jihan2519@naver.com>
2026-08-18 23:53:13 +0900

    Systems

---
+++ Assets/MemberWorkSpace/JJH/02_Scripts/Systems/ModuleSystem/Module.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace MemberWorkSpace.JJH._02_Scripts.Systems.ModuleSystem
+{
+    public abstract class Module : MonoBehaviour
+    {
+        protected ModuleOwner _owner;
+        public virtual void Initialize(ModuleOwner owner)
+        {
+            _owner = owner;
+        }
+    }
+}
