11eade73c4084399df74ecd8fbcfc62163a01d9e
ForRest1217 <Jihan2519@naver.com>
2026-09-01 09:10:37 +0900

    EnemyHealth

--- Assets/Members/JJH/02_Scripts/Agents/Agent.cs
+++ Assets/Members/JJH/02_Scripts/Agents/Agent.cs
@@ -1,13 +1,17 @@
 ﻿using Members.JJH._02_Scripts.Agents.Modules;
 using Members.JJH._02_Scripts.Systems.ModuleSystem;
+using RobotWeapons;
 using UnityEngine;
 
 namespace Members.JJH._02_Scripts.Agents
 {
-    public class Agent : ModuleOwner
+    public class Agent : ModuleOwner, IDamageable
     {
         public IRenderer Renderer { get; private set; }
         public ISensor Sensor { get; private set; }
+        public IHealth Health { get; private set; }
+
+        public bool IsAlive { get => Health.CurrentHealth > 0; }
 
         protected override void InitializeModules()
         {
@@ -17,6 +21,13 @@ namespace Members.JJH._02_Scripts.Agents
             Debug.Assert(Sensor != null, $"{gameObject.name}에는 ISensor모듈이 필요합니다.");
             Renderer = GetModule<IRenderer>();
             Debug.Assert(Renderer != null, $"{gameObject.name}에는 IRenderer모듈이 필요합니다.");
+            Health = GetModule<IHealth>();
+            Debug.Assert(Health != null, $"{gameObject.name}에는 IHealth모듈이 필요합니다.");
+        }
+
+        public void TakeDamage(float amount, GameObject source)
+        {
+            Health.TakeDamage(amount);
         }
     }
 }
---
+++ Assets/Members/JJH/02_Scripts/Agents/Modules/AgentHealth.cs
@@ -0,0 +1,32 @@
+﻿using Members.JJH._02_Scripts.Systems.ModuleSystem;
+using UnityEngine;
+
+namespace Members.JJH._02_Scripts.Agents.Modules
+{
+    public class AgentHealth : Module, IHealth
+    {
+        public float CurrentHealth
+        {
+            get => _health;
+            set
+            {
+                if (_health <= 0)
+                    _health = 0;
+                else
+                    _health = Mathf.Min(value, _maxHealth);
+            }
+        }
+        private float _health;
+
+        private float _maxHealth;
+
+        public void InitHealth(float maxHealth)
+        {
+            _maxHealth = maxHealth;
+            CurrentHealth = _maxHealth;
+        }
+
+        public void TakeDamage(float damage)
+            => CurrentHealth -= damage;
+    }
+}
