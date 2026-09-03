22c6b6d248acc0731488fd43cf1ff8555d195cb6
ForRest1217 <Jihan2519@naver.com>
2026-09-03 09:04:40 +0900

    EnemyGun

--- Assets/Members/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
+++ Assets/Members/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
@@ -5,55 +5,29 @@ using UnityEngine;
 
 namespace Members.JJH._02_Scripts.Agents.Enemies
 {
-    public abstract class AbstractEnemy : Agent, IWeaponOwner
+    public abstract class AbstractEnemy : Agent
     {
         [field: SerializeField] public EnemyDataSO EnemyData { get; private set; }
-
-        [Header("무기 관련")]
-        [SerializeField] private Transform aimOrigin;
-        [SerializeField] private Transform muzzleOrigin;
-        [SerializeField] private WeaponData equippedWeaponData;
-        [SerializeField] private WeaponHitbox weaponHitbox;
 
         public INavMesh EnemyNavMeshAgent { get; private set; }
         protected BehaviorGraphAgent BehaviorAgent { get; private set; }
 
-        public IWeapon Weapon { get; private set; }
-        public Transform AimOrigin => aimOrigin != null ? aimOrigin : transform;
-        public Transform MuzzleOrigin => muzzleOrigin != null ? muzzleOrigin : transform;
-
         protected override void InitializeModules()
         {
             base.InitializeModules();
+
             EnemyNavMeshAgent = GetModule<INavMesh>();
             Debug.Assert(EnemyNavMeshAgent != null, $"{gameObject.name}에는 INavMesh모듈이 필요합니다.");
             BehaviorAgent = GetComponent<BehaviorGraphAgent>();
             Debug.Assert(BehaviorAgent != null, $"{gameObject.name}에는 BehaviorGraphAgent가 필요합니다.");
+
             BehaviorAgent.SetVariableValue("Enemy", this);
             BehaviorAgent.SetVariableValue("AttackCooltime", EnemyData.AttackCooltime);
-
-            if (equippedWeaponData != null)
-            {
-                Weapon = WeaponFactory.Create(equippedWeaponData);
-                Weapon.Equip(this);
-                weaponHitbox?.Init(Weapon);
-            }
         }
 
-        protected virtual void Update()
-        {
-            Weapon?.Tick(Time.deltaTime);
-        }
-
-        public virtual void Attack()
-        {
-            Weapon?.PrimaryAttack();
-        }
+        public virtual void Attack() { }
 
         public void ApplyDamageTo(IDamageable target, float amount, bool isWeakpoint = false)
             => target?.TakeDamage(amount, gameObject);
-
-        public void SetWeaponHitboxActive(bool active)
-            => weaponHitbox?.SetActive(active);
     }
 }
--- Assets/Members/JJH/02_Scripts/Agents/Enemies/DronEnemy.cs
+++ Assets/Members/JJH/02_Scripts/Agents/Enemies/DronEnemy.cs
@@ -1,10 +1,6 @@
 ﻿namespace Members.JJH._02_Scripts.Agents.Enemies
 {
-    public class DronEnemy : AbstractEnemy
+    public class DronEnemy : AbstractGunEnemy
     {
-        public override void Attack()
-        {
-            base.Attack();
-        }
     }
 }
