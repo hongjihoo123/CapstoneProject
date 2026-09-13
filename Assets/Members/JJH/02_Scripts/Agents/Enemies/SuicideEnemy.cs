02d849307b6da1c19e21658dae0ed69e81e9826b
ForRest1217 <Jihan2519@naver.com>
2026-09-13 21:54:57 +0900

    Enemy_Suicide

--- Assets/Members/JJH/02_Scripts/Agents/Enemies/SuicideEnemy.cs
+++ Assets/Members/JJH/02_Scripts/Agents/Enemies/SuicideEnemy.cs
@@ -1,10 +1,17 @@
-﻿namespace Members.JJH._02_Scripts.Agents.Enemies
+﻿using UnityEngine;
+
+namespace Members.JJH._02_Scripts.Agents.Enemies
 {
     public class SuicideEnemy : AbstractEnemy
     {
+        [SerializeField] private GameObject deadParticle;
+
         public override void Attack()
         {
             base.Attack();
+            Weapon?.PrimaryAttack();
+            Instantiate(deadParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 }
